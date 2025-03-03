e41786a5b4593b11b0bad1bad3c5e824281ab7d8
mathsanu <9744917+mathsanu@users.noreply.github.com>
2025-03-02 23:59:48 -0500

    Add files via upload

---
+++ rise_and_shine.py
@@ -0,0 +1,7 @@
+def rise_and_shine():
+  """Prints a greeting message."""
+  print("Rise and shine!")
+  print ("mats here ")
+if __name__ == "__main__":
+  rise_and_shine()
+  print("Rise and shine2!")
