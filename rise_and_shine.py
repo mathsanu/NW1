e9148eac78242f5eb06cc780a45a669809cc3d81
mathsanu <9744917+mathsanu@users.noreply.github.com>
2025-03-03 05:07:09 +0000

    Add Iam

--- rise_and_shine.py
+++ rise_and_shine.py
@@ -2,6 +2,7 @@ def rise_and_shine():
   """Prints a greeting message."""
   print("Rise and shine!")
   print ("mats here ")
+  print ("I am here ")
 if __name__ == "__main__":
   rise_and_shine()
   print("Rise and shine2!")
